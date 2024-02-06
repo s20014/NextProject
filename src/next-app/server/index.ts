15f70b9099b2d6c5d7dba6b2fc111379b0ecaf0a
unko <unko@example.com>
2024-02-06 23:46:06 +0900

    うんこ

--- src/next-app/server/index.ts
+++ src/next-app/server/index.ts
@@ -1,21 +1,22 @@
 import express, { Request, Response } from "express";
 import next from "next";
- 
-const dev = process.env.NODE_ENV !== "production";
+
+const dev = process.env.NODE_ENV === "development";
+const port = process.env.PORT || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
-const port = process.env.PORT || 3000;
- 
-app.prepare().then(() => {
-	const server = express();
- 
-	server.all("*", (req: Request, res: Response) => {
-		return handle(req, res);
-	});
-	server.listen(port, (err?: any) => {
-		if (err) throw err;
-		console.log(
-                      `> Ready on localhost:${port} - env ${process.env.NODE_ENV}`
-                );
-	});
-});
+
+(async () => {
+  try {
+    await app.prepare();
+    const server = express();
+    server.all("*", (req: any, res: any) => {
+      return handle(req, res);
+    });
+    server.listen(port, () => {
+      console.log(`${port}で起動中`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+})();
