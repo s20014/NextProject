66f4499d952d8f1d50308afeefae90f1d7a35180
zenfuku667 <zenfuku667@gmail.com>
2024-02-08 18:14:33 +0900

    aiueo

--- src/next-app/server/index.ts
+++ src/next-app/server/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import next from "next";
+import { ResolvableTo } from "tailwindcss/types/config";
 
 const dev = process.env.NODE_ENV === "development";
 const port = process.env.PORT || 3000;
@@ -10,12 +11,15 @@ const handle = app.getRequestHandler();
   try {
     await app.prepare();
     const server = express();
-    server.all("*", (req: any, res: any) => {
+
+    server.all("*", (req: Request, res: Response) => {
       return handle(req, res);
     });
+
     server.listen(port, () => {
       console.log(`${port}で起動中`);
     });
+
   } catch (e) {
     console.error(e);
   }
---
+++ src/next-app/src/app/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { useRouter } from "next/navigation";
+
+export default function Header() {
+
+    return (
+        <div className="headerArea">
+            aaaaa
+        </div>
+    );
+};
